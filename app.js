571e87764c3101e42a8706f77e2101a878fc0acb
ns639p <narenrsk@gmail.com>
2024-12-03 21:04:39 +0530

    day 36

--- app.js
+++ app.js
@@ -12,6 +12,30 @@ app.get('/api/v1/movies',(req,res)=>{
         }
     })
 })
+
+
+//GET - /api/v1/movies/id
+app.get('/api/v1/movies/:id?',(req,res)=>{
+    //console.log(req.params);
+    const id = req.params.id * 1; // the id is stored as a string in request.params multiplying it by 1 will turn it into a number or you the add symbol in front like this +req.params.id
+    let movie = movies.find(el=>el.id===id)
+
+    if (!movie){
+        return res.status(404).json({
+            status:'Failed',
+            message:'Movie with ID ' +id+ ' is not found'
+        })
+    }
+
+    res.status(200).json({
+        status:"success",
+        data:{
+            movie:movie
+        }
+    })
+})
+
+
 app.post('/api/v1/movies',(req,res)=>{
     const newId = movies[movies.length-1].id+1;
     const newMovie = Object.assign({id:newId},req.body)
