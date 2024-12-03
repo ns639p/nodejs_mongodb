4028c81e412483c2e3062b8b0335d99ccea613d5
ns639p <narenrsk@gmail.com>
2024-12-03 18:57:14 +0530

    day 35

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const fs = require('fs')
 const app = express()
 const port = 1000
-app.get('/',(req,res)=>{
-    res.status(500).json({message:'Welcome to express js'})
+const movies =  JSON.parse(fs.readFileSync('./data/movies.json'));
+app.use(express.json())
+app.get('/api/v1/movies',(req,res)=>{
+    res.status(200).json({
+        status:"success",
+        data:{
+            movies:movies
+        }
+    })
 })
-app.post('/',()=>{
-    
+app.post('/api/v1/movies',(req,res)=>{
+    const newId = movies[movies.length-1].id+1;
+    const newMovie = Object.assign({id:newId},req.body)
+    movies.push(newMovie);
+
+    fs.writeFile('./data/movies.json',JSON.stringify(movies),(err)=>{
+        res.status(201).json({
+            status:"success",
+            data:{
+                movie:newMovie
+            }
+        })
+    })
+    //res.send('created')
 })
+
+
 app.listen(port,()=>{
     console.log('starting server....')
 })
