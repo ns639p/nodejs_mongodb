66feb859715433598408eec657eee2c830f51549
ns639p <narenrsk@gmail.com>
2024-12-04 16:22:22 +0530

    day 41

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const fs = require('fs')
+const morgan = require('morgan')//this is a HTTP logging middleware
 const app = express()
 const port = 1000
 const movies =  JSON.parse(fs.readFileSync('./data/movies.json'));
 
 const logger = function(req,res,next){
-    console.log('custom middleware caller');
+    console.log('custom middleware called');
     next()
 }
 
 app.use(express.json())
-app.use(logger)
+app.use(morgan('dev'))//morgan is not a middle ware this is a function that returns a middle ware. Search 'morgan github' in google and open the index.js file in line 59 you will see the morgan functon taking in only two parameters but it returns a middleware function called logger (line 103) that takes three parameters req,res and next
+app.use(logger)//the above two have () but this doesnt why ? because this is a middleware function and takes three parameters req,res,next
 app.use((req,res,next)=>{
     req.requestedAt = new Date().toISOString();
     next();
