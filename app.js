a759996b459270602849658bfc4c6b7ba21698fe
ns639p <narenrsk@gmail.com>
2024-12-04 09:46:23 +0530

    day 38

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const app = express()
 const port = 1000
 const movies =  JSON.parse(fs.readFileSync('./data/movies.json'));
 app.use(express.json())
-app.get('/api/v1/movies',(req,res)=>{
+
+
+//Route handler function 
+const getAllMovies = (req,res)=>{
     res.status(200).json({
         status:"success",
         data:{
             movies:movies
         }
     })
-})
+}
 
-
-//GET - /api/v1/movies/id
-app.get('/api/v1/movies/:id?',(req,res)=>{
+const getMovie = (req,res)=>{
     //console.log(req.params);
     const id = req.params.id * 1; // the id is stored as a string in request.params multiplying it by 1 will turn it into a number or you the add symbol in front like this +req.params.id
     let movie = movies.find(el=>el.id===id)
@@ -33,10 +34,10 @@ app.get('/api/v1/movies/:id?',(req,res)=>{
             movie:movie
         }
     })
-})
+}
 
 
-app.post('/api/v1/movies',(req,res)=>{
+const createMovie = (req,res)=>{
     const newId = movies[movies.length-1].id+1;
     const newMovie = Object.assign({id:newId},req.body)
     movies.push(newMovie);
@@ -50,15 +51,15 @@ app.post('/api/v1/movies',(req,res)=>{
         })
     })
     //res.send('created')
-})
+}
 
 
-app.patch('/api/v1/movies/:id',(req,res)=>{
-    console.log('receiving request')
+const updateMovie = (req,res)=>{
+    
     const id = req.params.id * 1;
     const movieToUpdate = movies.find(el=>el.id===id);
     if (!movieToUpdate){
-        console.log('hits for 404')
+        
         return res.status(404).json({
             status:'failed',
             message:'Movie with ID ' +id+ ' is not found'
@@ -68,7 +69,7 @@ app.patch('/api/v1/movies/:id',(req,res)=>{
     Object.assign(movieToUpdate,req.body);//we are passing two objects to Object.assign what happens is the two object will be merged if thy have the same properties then no problem if they have the different properties the property of second object will be implemented
     movies[index] = movieToUpdate
     fs.writeFile('./data/movies.json',JSON.stringify(movies),(err)=>{
-        console.log('hits write file')
+        
         res.status(200).json({
             status:'success',
             data:{
@@ -76,7 +77,51 @@ app.patch('/api/v1/movies/:id',(req,res)=>{
             }
         })
     })
-})
+}
+
+
+const deleteMovie = (req,res)=>{
+    const id = req.params.id * 1;
+    const movieToDelete = movies.find(el=>el.id===id);
+
+    if (!movieToDelete){
+        
+        return res.status(404).json({
+            status:'failed',
+            message:'Movie with ID ' +id+ ' is not found to delete'
+        })
+    }
+    const index = movies.indexOf(movieToDelete);
+
+    movies.splice(index,1)
+
+    fs.writeFile('./data/movies.json',JSON.stringify(movies),(err)=>{
+        console.log('hits write file')
+        res.status(204).json({
+            status:'success',
+            data:{
+                movie:null
+            }
+        })
+    })
+}
+
+    // app.get('/api/v1/movies',getAllMovies)
+    // app.get('/api/v1/movies/:id',getMovie)
+    // app.post('/api/v1/movies',createMovie)
+    // app.patch('/api/v1/movies/:id',updateMovie)
+    // app.delete('/api/v1/movies/:id',deleteMovie)
+
+
+app.route('/api/v1/movies')
+    .get(getAllMovies)
+    .post(createMovie)
+
+app.route('/api/v1/movies/:id')
+    .get(getMovie)
+    .patch(updateMovie)
+    .delete(deleteMovie)
+
 
 app.listen(port,()=>{
     console.log('starting server....')
