dbaac83f3436bc3535b250124d8ec2639104d9a0
ns639p <narenrsk@gmail.com>
2024-12-06 18:08:05 +0530

    day 69

--- Controllers/moviesController.js
+++ Controllers/moviesController.js
@@ -4,10 +4,39 @@ const Movie = require('./../Models/movieModel')
 
 
 
-exports.getAllMovies = (req,res)=>{
+exports.getAllMovies = async(req,res)=>{
+    try{
+        const movies = await Movie.find();
+        res.status(200).json({
+            status:'success',
+            length:movies.length,
+            data:{
+                movies
+            }
+        })
+    }catch(err){
+        res.status(400).json({
+            status:'failed',
+            message:err.message
+        })
+    }
 }
 
-exports.getMovie = (req,res)=>{
+exports.getMovie = async(req,res)=>{
+    try{
+        const movie = await Movie.findById(req.params.id);
+        res.status(200).json({
+            status:'success',
+            data:{
+                movie
+            }
+        })
+    }catch(err){
+        res.status(400).json({
+            status:'failed',
+            message:err.message
+        })
+    }
 }
 
 
