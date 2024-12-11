d3336eac96f771b57503d950a28b32a72f02ad94
ns639p <narenrsk@gmail.com>
2024-12-11 20:49:08 +0530

    day 90

--- app.js
+++ app.js
@@ -21,7 +21,13 @@ app.use((req,res,next)=>{
 })
 
 
-
 app.use('/api/v1/movies',moviesRouter)
+
+app.all('*',(req,res,next)=>{
+    res.status(404).json({
+        status:'failed',
+        message:`Can't find ${req.originalUrl} on the server`
+    })
+})
 
 module.exports = app;
