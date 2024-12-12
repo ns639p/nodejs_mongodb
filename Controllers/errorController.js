4f75ddfd004e4a18cff28367b5f342c07477b21e
ns639p <narenrsk@gmail.com>
2024-12-12 18:30:43 +0530

    day 96

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,9 +1,42 @@
+const CustomError = require('./../utils/CustomError')
+function developmentError(res,error){
+    res.status(error.statusCode).json({
+        status:error.status,
+        message:error.message,
+        stackTrace:error.stack,
+        error:error
+    })
+}
+
+function productionError(res,error){
+    if (error.isOperational){
+        res.status(error.statusCode).json({
+        status:error.status,
+        message:error.message
+        })
+    }else{
+        res.status(500).json({
+            status:'error',
+            message:'Something went wrong buddy boy! please try again'
+            })
+    }
+}
+
+
+function castErrorHandler (error){
+    const msg = `Invalid value for ${error.path}: ${error.value}` 
+    return new CustomError(msg,400);
+}
 module.exports = (error,req,res,next)=>{
     error.statusCode = error.statusCode||500;
     error.status=error.status||'error';
-    res.status(error.statusCode).json({
-        status:error.status,
-        message:error.message
-    })
+    if (process.env.NODE_ENV === 'development'){
+        developmentError(res,error)
+    }else if (process.env.NODE_ENV==='production'){
+        if (error.name === 'CastError'){
+            error = castErrorHandler(error);
+        }
+        productionError(res,error);
+    }
 }
 
