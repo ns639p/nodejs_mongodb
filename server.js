151ebb8e25468576269385a9cf208b781d3c2e02
ns639p <narenrsk@gmail.com>
2024-12-12 21:05:45 +0530

    day 100

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config({path:'./config.env'})
 
+
+process.on('uncaughtException',(err)=>{
+  console.log(err.name,err.message);
+  console.log('Uncaught exception occured! shutting down...');
+  process.exit(1);
+})
+
 const app = require('./app')
 
 // console.log(app.get('env'))
