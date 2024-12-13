28172da530a7ade5e5d56a267f05299690e69710
ns639p <narenrsk@gmail.com>
2024-12-13 19:41:45 +0530

    day 106

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,10 +1,16 @@
 const User = require('./../Models/userModel')
 const asyncErrorHandler = require('./../utils/asyncErrorHandler')
+const jwt = require('jsonwebtoken')
 
 exports.signup = asyncErrorHandler(async(req,res,next)=>{
     const newUser = await User.create(req.body);
+
+    const token = jwt.sign({id:newUser._id},process.env.SECRET_STR,{
+        expiresIn:process.env.EXPIRESIN
+    })
     res.status(201).json({
         status:'success',
+        token,
         data:{
             user:newUser
         }
